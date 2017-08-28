ea83cfa9f45d575f99eaf1658b6a3d44fb677ae8
DESKTOP-IMS57EF\hujing <2851352930@qq.com>
2017-08-28 18:01:09 +0800

    USB插入移除通知

--- Usb/UsbNotifier.cs
+++ Usb/UsbNotifier.cs
@@ -50,9 +50,21 @@ namespace Usb
 
         private readonly ManagementEventWatcher _removeWatcher;
 
+        private static readonly ManagementScope Scope;
+
+        private static readonly TimeSpan Interval = TimeSpan.FromSeconds(3);
+
+        private Boolean _isStarted;
+
         #endregion
 
         #region Constructors
+
+        static UsbNotifier()
+        {
+            Scope = new ManagementScope("root\\CIMV2");
+            Scope.Options.EnablePrivileges = true;
+        }
 
         private UsbNotifier()
         {
@@ -69,7 +81,53 @@ namespace Usb
         public static UsbNotifier OpenNotifier()
         {
             UsbNotifier notifier = new UsbNotifier();
+            notifier.Init();
             return notifier;
+        }
+
+        public void Open()
+        {
+            if (_isStarted) return;
+            _insertWatcher.Start();
+            _removeWatcher.Start();
+        }
+
+        public void Close()
+        {
+            if (!_isStarted) return;
+            _insertWatcher.Stop();
+            _removeWatcher.Stop();
+        }
+
+        #endregion
+
+        #region Private
+
+        private void Init()
+        {
+            WqlEventQuery insertQuery = new WqlEventQuery("__InstanceCreationEvent", Interval, "TargetInstance isa 'Win32_USBControllerDevice'");
+            _insertWatcher.Scope = Scope;
+            _insertWatcher.Query = insertQuery;
+            _insertWatcher.EventArrived += _insertWatcher_EventArrived;
+            _insertWatcher.Start();
+
+            WqlEventQuery removeQuery = new WqlEventQuery("__InstanceDeletionEvent", Interval, "TargetInstance isa 'Win32_USBControllerDevice'");
+            _removeWatcher.Scope = Scope;
+            _removeWatcher.Query = removeQuery;
+            _removeWatcher.EventArrived += _removeWatcher_EventArrived;
+            _removeWatcher.Start();
+
+            _isStarted = true;
+        }
+
+        private void _removeWatcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            OnRemoved();
+        }
+
+        private void _insertWatcher_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            OnInserted();
         }
 
         #endregion
